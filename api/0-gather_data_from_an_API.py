e9dc781133d43b6d491799e01a2dee7c17f1fd9f
jocalao001 <113933963+jocalao001@users.noreply.github.com>
2023-11-28 22:27:46 -0500

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -13,12 +13,12 @@ if __name__ == "__main__":
     tr = []
     for dicc in to.json():
         for k, v in dicc.items():
-            if k == "completed":
+            if k == 'completed':
                 if v is True:
                     tr.append(dicc)
     print(f"Employee {us.json().get('name')} is \
 done with tasks({(len(tr))}/{(len(to.json()))}):")
     for tittle in tr:
         for k, v in tittle.items():
-            if k == "title":
-                print(f"	 {v}")
+            if k == 'title':
+                print(f'	 {v}')
